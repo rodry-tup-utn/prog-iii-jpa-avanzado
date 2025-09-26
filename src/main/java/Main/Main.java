2346c76a3690c6fc124b89258279f837f7e2d069
rodrymza <rodrymza@gmail.com>
2025-09-25 23:45:30 -0300

    Cambio de paquete de Main

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,53 @@
+package Main;
+
+import DAO.PacienteDAO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.rodry.Entity.HistoriaClinica;
+import org.rodry.Entity.Paciente;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("practicoJPA");
+        EntityManager em = emf.createEntityManager();
+        PacienteDAO pacienteDao = new PacienteDAO(em, Paciente.class);
+        
+        try {
+            HistoriaClinica clinica = new HistoriaClinica("Historia clinica del paciente Ramirez");
+            HistoriaClinica clinica1 = new HistoriaClinica("Historia clinica del paciente Arancibia");
+            Paciente paciente = Paciente.builder()
+                                        .nombre("Rodrigo")
+                                        .apellido("Ramirez")
+                                        .edad(34)
+                                        .obraSocial("Boreal")
+                                        .dni("36499229")
+                                        .historiaClinica(clinica)
+                                        .build();
+            Paciente paciente2 = Paciente.builder()
+                                         .nombre("Rocio")
+                                         .apellido("Arancibia")
+                                         .edad(31)
+                                         .obraSocial("Boreal")
+                                         .dni("39473146")
+                                         .historiaClinica(clinica1)
+                                         .build();
+            
+            pacienteDao.guardar(paciente);
+            pacienteDao.guardar(paciente2);
+            
+            List<Paciente> listaPacientes = pacienteDao.listarMayoresDe(30);
+            listaPacientes.forEach(System.out::println);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
+
