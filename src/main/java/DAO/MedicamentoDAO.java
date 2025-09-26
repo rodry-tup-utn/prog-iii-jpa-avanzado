e9866d4d47b1b85e575fad796df4bd6b3b750076
rodrymza <rodrymza@gmail.com>
2025-09-26 11:29:19 -0300

    arreglo en metodo que lista medicamentos por paciente

--- src/main/java/DAO/MedicamentoDAO.java
+++ src/main/java/DAO/MedicamentoDAO.java
@@ -7,18 +7,16 @@ import org.rodry.Entity.Paciente;
 import java.util.List;
 
 public class MedicamentoDAO extends GenericDAO<Medicamento> {
-    EntityManager em;
-
-    public MedicamentoDAO(EntityManager em, Class<Medicamento> claseEntidad, EntityManager em1) {
+    
+    public MedicamentoDAO(EntityManager em, Class<Medicamento> claseEntidad) {
         super(em, claseEntidad);
-        this.em = em1;
     }
-
+    
     //5 Mostrar todos los medicamentos asociados a un paciente
     public List<Medicamento> listarMedicamentosPaciente(Paciente paciente) {
         return em.createQuery("select m from Medicamento m" +
                         " join m.listaPaciente p " +
-                        "where m.paciente = :paciente", Medicamento.class)
+                        "where p = :paciente", Medicamento.class)
                 .setParameter("paciente", paciente)
                 .getResultList();
     }
